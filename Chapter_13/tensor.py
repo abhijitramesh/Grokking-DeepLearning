6e252d12eea5d6d5dc0e6d1eb8e4c52866d4467c
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2021-02-10 15:54:39 +0530

    Notebook Introduction to auto grad, Adds: Initial tensor code with autograd for addition

---
+++ Chapter_13/tensor.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class Tensor(object):
+
+    def __init__(self,data,creators=None,creation_op=None):
+        self.data = np.array(data)
+        self.creation_op = creation_op
+        self.creators = creators
+        self.grad = None
+
+    def backward(self,grad):
+        self.grad = grad
+
+        if(self.creation_op =="add"):
+            self.creators[0].backward(grad)
+            self.creators[1].backward(grad)
+
+    def __add__(self,other):
+        return Tensor(self.data+other.data,
+                      creators=[self,other],
+                      creation_op="add")
+
+    def __repr__(self):
+        return str(self.data.__repr__())
+
+    def __str__(self):
+        return str(self.data.__str__())
+
+
+
